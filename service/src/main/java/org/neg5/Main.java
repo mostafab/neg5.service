3544da246c3c081175e0971c6bfd4fc78b9a8d0d
mostafab <mostafa0104@gmail.com>
2018-10-09 21:12:06 -0700

    Fix db config and load from environment variables.

--- service/src/main/java/org/neg5/Main.java
+++ service/src/main/java/org/neg5/Main.java
@@ -3,11 +3,13 @@ package org.neg5;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.neg5.module.DataAccessModule;
+import org.neg5.module.SystemPropertiesModule;
 
 public class Main {
 
     public static void main(String args[]) throws Exception {
         Injector injector = Guice.createInjector(
+                new SystemPropertiesModule(),
                 new DataAccessModule(),
                 new RouterModule(),
                 new FilterModule()
