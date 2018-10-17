38550f37c99efd703f5bad984c0ad797ef786a6b
mostafab <mostafa0104@gmail.com>
2018-10-16 21:41:25 -0700

    Add mappers for match raw query

--- core/src/main/java/org/neg5/managers/TournamentMatchManager.java
+++ core/src/main/java/org/neg5/managers/TournamentMatchManager.java
@@ -3,6 +3,7 @@ package org.neg5.managers;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.neg5.TournamentMatchDTO;
+import org.neg5.TournamentTossupValueDTO;
 import org.neg5.core.TransactionalSimple;
 import org.neg5.daos.TournamentMatchDAO;
 import org.neg5.data.TournamentMatch;
@@ -11,10 +12,14 @@ import org.neg5.mappers.TournamentMatchMapper;
 import org.neg5.mappers.data.MatchToMatchDTOMapper;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Singleton
 public class TournamentMatchManager extends AbstractManager<TournamentMatch, TournamentMatchDTO> {
+
+    @Inject private TournamentManager tournamentManager;
 
     @Inject private TournamentMatchMapper tournamentMatchMapper;
     @Inject private MatchToMatchDTOMapper matchToMatchDTOMapper;
@@ -32,8 +37,9 @@ public class TournamentMatchManager extends AbstractManager<TournamentMatch, Tou
 
     @Override
     public List<TournamentMatchDTO> findAllByTournamentId(String tournamentId) {
+        Map<Integer, TournamentTossupValueDTO> tossupValues = getTossupValueMap(tournamentId);
         return findByRawQuery(tournamentId).stream()
-                .map(matchToMatchDTOMapper::toDTO)
+                .map(match -> matchToMatchDTOMapper.toDTO(match, tossupValues))
                 .collect(Collectors.toList());
     }
 
@@ -46,5 +52,10 @@ public class TournamentMatchManager extends AbstractManager<TournamentMatch, Tou
     @TransactionalSimple
     protected List<Match> findByRawQuery(String tournamentId) {
         return tournamentMatchDAO.findMatchesByTournamentIdWithRawQuery(tournamentId);
+    }
+
+    private Map<Integer, TournamentTossupValueDTO> getTossupValueMap(String tournamentId) {
+        return tournamentManager.get(tournamentId).getTossupValues().stream()
+                .collect(Collectors.toMap(TournamentTossupValueDTO::getValue, Function.identity()));
     }
 }
--- core/src/main/java/org/neg5/mappers/data/MatchToMatchDTOMapper.java
+++ core/src/main/java/org/neg5/mappers/data/MatchToMatchDTOMapper.java
@@ -3,10 +3,12 @@ package org.neg5.mappers.data;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.neg5.TournamentMatchDTO;
+import org.neg5.TournamentTossupValueDTO;
 import org.neg5.data.transformers.data.Match;
 import org.neg5.data.transformers.data.Phase;
 import org.neg5.mappers.AbstractObjectMapper;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Singleton
@@ -18,11 +20,20 @@ public class MatchToMatchDTOMapper extends AbstractObjectMapper<Match, Tournamen
         super(Match.class, TournamentMatchDTO.class);
     }
 
-    @Override
-    public TournamentMatchDTO toDTO(Match match) {
+    public TournamentMatchDTO toDTO(Match match, Map<Integer, TournamentTossupValueDTO> tossupValues) {
         TournamentMatchDTO dto = super.toDTO(match);
         dto.setPhases(match.getPhases().stream().map(Phase::getId).collect(Collectors.toSet()));
         dto.setTeams(match.getTeams().stream().map(teamInMatchMapper::toDTO).collect(Collectors.toSet()));
+
+        dto.getTeams().stream()
+                .flatMap(team -> team.getPlayers().stream())
+                .flatMap(player -> player.getAnswers().stream())
+                .forEach(answer -> {
+                    if (tossupValues.containsKey(answer.getTossupValue())) {
+                        answer.setAnswerType(tossupValues.get(answer.getTossupValue()).getAnswerType());
+                    }
+                });
+
         return dto;
     }
 }
