435cd62b44ed6d5f240bb18f656ba020808c38ae
mostafab <mostafa0104@gmail.com>
2018-11-13 11:53:07 -0800

    Tests and user filter

--- service/src/main/java/org/neg5/FilterModule.java
+++ service/src/main/java/org/neg5/FilterModule.java
@@ -3,7 +3,6 @@ package org.neg5;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 
-import org.neg5.filters.CurrentUserContextFilter;
 import org.neg5.filters.NoResultHandlerFilter;
 import org.neg5.filters.RequestFilter;
 
@@ -14,6 +13,5 @@ public class FilterModule extends AbstractModule {
         Multibinder<RequestFilter> multibinder = Multibinder.newSetBinder(binder(), RequestFilter.class);
 
         multibinder.addBinding().to(NoResultHandlerFilter.class);
-        multibinder.addBinding().to(CurrentUserContextFilter.class);
     }
 }
