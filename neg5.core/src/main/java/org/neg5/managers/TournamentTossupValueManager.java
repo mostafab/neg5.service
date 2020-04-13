c5a904b8a2df1aa4f51dc6b32aa9299ad60ad6f1
mostafab <mostafa0104@gmail.com>
2020-04-12 20:48:21 -0700

    Player controller

--- neg5.core/src/main/java/org/neg5/managers/TournamentTossupValueManager.java
+++ neg5.core/src/main/java/org/neg5/managers/TournamentTossupValueManager.java
@@ -1,12 +1,14 @@
 package org.neg5.managers;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import org.neg5.TournamentTossupValueDTO;
 import org.neg5.daos.TournamentTossupValueDAO;
 import org.neg5.data.TournamentTossupValue;
 import org.neg5.data.embeddables.TournamentTossupValueId;
 import org.neg5.mappers.TournamentTossupValueMapper;
 
+@Singleton
 public class TournamentTossupValueManager extends
         AbstractDTOManager<TournamentTossupValue, TournamentTossupValueDTO, TournamentTossupValueId> {
 
